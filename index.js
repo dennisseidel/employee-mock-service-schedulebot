d76f209a5443c91be06c29a56c5a0613ea55bf7f
Dennis Seidel <mail@dennisseidel.de>
2017-01-21 19:08:53 +0100

    feat: setup server

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+
+const port = process.env.PORT || 3001;
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Server is listening to port ${port}`);
+});
