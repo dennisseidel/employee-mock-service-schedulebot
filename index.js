64de843c8147a18a880e98c04e9e0eacbbcdb054
Dennis Seidel <mail@dennisseidel.de>
2017-01-21 21:09:21 +0100

    feat: create express operations

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const express = require('express');
 const port = process.env.PORT || 3001;
 const app = express();
 
+// Operation to query employee data
+app.get('/employees', (req, res) => {
+  console.log(res);
+});
+
+// Operation to create employees
+app.post('/employees', (req, res) => {
+  
+});
+
 app.listen(port, () => {
   console.log(`Server is listening to port ${port}`);
 });
